9891276470775168e05d655104ecbc0d960186ed
Lahiru-Senadheera <lahiru.nirmal.senadeera@gmail.com>
2025-08-23 22:22:34 +0530

    fixing Overflow issue

--- lib/widgets/modern_card.dart
+++ lib/widgets/modern_card.dart
@@ -18,6 +18,7 @@ class ModernCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       decoration: BoxDecoration(
         color: color ?? ModernTheme.surfaceVariant,
         borderRadius: BorderRadius.circular(16),
@@ -39,6 +40,34 @@ class ModernCard extends StatelessWidget {
             padding: padding ?? const EdgeInsets.all(16),
             child: child,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ModernStatusChip extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  const ModernStatusChip({Key? key, required this.text, required this.color})
+    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: color.withOpacity(0.3)),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: color,
+          fontWeight: FontWeight.w600,
+          fontSize: 12,
         ),
       ),
     );
